459a3768025c7f8fe50908125d41937e5bb2bc63
Sankalp-sa <sankalp.p@ahduni.edu.in>
2024-07-22 01:13:38 +0530

    changing cors policy and still trying to fix res.cookie domain error

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ io.on("connection", (socket) => {
     });
 });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://summerprojectfrontend.onrender.com", credentials: true }));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
